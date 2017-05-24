80069f42bd44e314c17f53fb4f2c7e533721b426
Broto <broto@Brotos-MacBook-Pro.local>
2017-05-24 02:07:05 -0300

    - Removed Databinding support duo conflict with kotlin and dagger annotation processors

--- app/src/main/kotlin/com/abraaolima/safechat/SafeChatApplication.kt
+++ app/src/main/kotlin/com/abraaolima/safechat/SafeChatApplication.kt
@@ -3,6 +3,7 @@ package com.abraaolima.safechat
 import android.app.Application
 import com.abraaolima.safechat.dagger.AppComponent
 import com.abraaolima.safechat.dagger.AppModule
+import com.abraaolima.safechat.dagger.DaggerAppComponent
 
 /**
  * Created by broto on 23/05/17.
--- app/src/main/kotlin/com/abraaolima/safechat/screens/login/signin/SignInActivity.kt
+++ app/src/main/kotlin/com/abraaolima/safechat/screens/login/signin/SignInActivity.kt
@@ -1,24 +1,22 @@
 package com.abraaolima.safechat.screens.login.signin
 
-import android.databinding.DataBindingUtil
 import android.os.Bundle
 import android.support.design.widget.Snackbar
 import android.support.v7.app.AppCompatActivity
 import com.abraaolima.safechat.R
-import com.abraaolima.safechat.databinding.ActivitySigninBinding
-import javax.inject.Inject
+import kotlinx.android.synthetic.main.activity_signin.*
 
 class SignInActivity : AppCompatActivity() {
 
-    @Inject
-    lateinit var viewModel: SignInViewModel
+//    @Inject
+//    lateinit var viewModel: SignInViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        val binding: ActivitySigninBinding = DataBindingUtil.setContentView(this, R.layout.activity_signin)
+        setContentView(R.layout.activity_signin)
 
-        binding.fabSiginShowRegister.setOnClickListener { view ->
+        fab_sigin_show_register.setOnClickListener { view ->
             Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                     .setAction("Action", null).show()
         }
